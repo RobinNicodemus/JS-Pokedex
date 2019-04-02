1a0305fdfc287e2766f4fc921a91f5805ef4a044
Robin Dippold <RobinDippold@users.noreply.github.com>
2019-04-02 16:11:31 +0200

    Rewritten w/ functional programming in mind
    Wrapped the repository in an IIFE. It contains 3 public functions: Search, add, getAll.
    
    The data should be presented in a readable way in the browser.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,115 @@
-var repository = [];
-
-pokemon_0 = {
-	name: 'Bulbasaur',
-	height: 0.7,           //height data stored in meters
-	types: ['grass']
-}; 
-
-pokemon_1 = {
-	name: 'Charmander',
-	height: 0.6,
-	types: ['fire']
-}; 
-
-pokemon_2 = {
-	name: 'Squirtle',
-	height: 0.5,
-	types: ['water']
-};
-
-console.log(repository.length);
-repository.push(pokemon_0);
-repository.push(pokemon_1);
-repository.push(pokemon_2);
-console.log(repository.length);
-
-
-//document.write content could be merged, but i split it for better readability. 
-for (var i = 0; i <= 2; i++) {
-	document.write('<br />', repository[i].name); //the <br /> has only aesthetic function and should be replaced with something better soon.
-	document.write("(" + repository[i].height + "m, types: "); 
-	//Here i try to change the background color of the types display
-	if (repository[i].types[0] === 'grass') {
-		document.write('<span class="green_bg">', repository[i].types, '</span>')
-		document.write(")");
-	} else if (repository[i].types[0] === 'water') {
-		document.write('<span class="blue_bg">', repository[i].types, '</span>')
-		document.write(")");
-	} else if (repository[i].types[0] === 'fire') {
-		document.write('<span class="red_bg">', repository[i].types, '</span>')
-		document.write(")");
-	} else { 
-		document.write(repository[i].types + ")")
-	}
-
-	if (repository[i].height >= 0.7) {
-		document.write(" Wow, that`s big!");
-	}
-		else if (repository[i].height <= 0.5) {
-			document.write(" Look how cute it is!");
+var pokemonRepository = (function () {
+	var repository = [
+		{
+			name: 'AllTypeTestMon',
+			height: 0.6,
+			types: ['water','fire','grass']
+		},
+		{
+			name: 'Bulbasaur',
+			height: 0.7,         
+			types: ['grass']
+		},
+		{
+			name: 'Charmander',
+			height: 0.6,
+			types: ['fire']
+		},
+		{
+			name: 'Squirtle',
+			height: 0.5,
+			types: ['water']
+		},
+	];
+ 
+//it uses - isTrueEqualKeysObj -(see below) to compare the new entry with the first one in the array.
+//returns what is wrong if something does not fit.
+	function add(pokemon){
+		if (typeof(pokemon) === 'object') {
+			if (isTrueEqualKeysObj (pokemonRepository.getAll()[0], pokemon)) {
+				repository.push(pokemon);
+			} else {
+				console.log('Please ensure your pokemon has the correct format: {name: (string), height: (number), types: [array of strings]}');
+			}
+		} else {
+			console.log('Please add an object');
 		}
+	}
+
+//filter the repository by checking whether a given string in lowercase is part ...
+//of the value (in lowercase) of the name property of objects in the repository.
+//returns an array of objects whose name value correspond to the given string or an empty one
+	function searchByName(inputNameString) {
+		return repository.filter(function (repositoryObject){
+			return repositoryObject.name.toLowerCase().indexOf(inputNameString.toLowerCase()) != -1;
+		})
+	}
+
+	function getAll() {
+		return repository;
+	}
+
+	return {
+		add: add,
+		getAll: getAll,
+		searchByName: searchByName
+	};
+})();
+
+//----------------------------------------------------------------------------------------------------
+//compares object keys after checking that the number of keys in two objects is the same.
+//does not work with objects that have functions as properties.
+function isTrueEqualKeysObj (obj1, obj2) {
+	if (Object.keys(obj1).length === Object.keys(obj2).length) {
+		var i = 0;
+		while (i < Object.keys(obj1).length && (Object.keys(obj1)[i] === Object.keys(obj2)[i])) {
+			i++;
+		}
+		if (i === Object.keys(obj1).length) { //if the number of corresponding keys is equal to the number of keys, return true
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+//---------------------------------------------------------------------------------------------------
+var allPokemon = pokemonRepository.getAll();
+
+function getHeightDescription(singlePokemon) {
+	return singlePokemon.height + 'm';
+}
+
+function getHeightEmphasis(singlePokemon) {
+	if (singlePokemon.height >= 0.7) {
+		return 'Look how big it is!';
+	} else if (singlePokemon.height <= 0.5){
+		return 'It is so cute!';
+	} else {
+		return null;   //if the pokemon is not special regarding height the function should return something falsy
+	}
+}
+
+function getTypesDescription(singlePokemon) {
+	return singlePokemon.types;
+}
+
+function getPokeDescription(singlePokemon) {
+	return singlePokemon.name + ' (' + getHeightDescription(singlePokemon) + ', ' + getTypesDescription(singlePokemon) + ')';
+}
+
+//if getHeightEmphasis() returns something truthy, return the pokedescription and the emphasis, with fitting characters as separation.
+//otherwise return only the pokedescription.
+function getForPrintPokeDescription(singlePokemon) {
+	if (getHeightEmphasis(singlePokemon)) {   
+		return getPokeDescription(singlePokemon) + ' - ' + getHeightEmphasis(singlePokemon);
+	} else {
+		return getPokeDescription(singlePokemon);
 	} 
+}
+
+allPokemon.forEach(function (pokemon) {
+	document.write(getForPrintPokeDescription(pokemon) + '<br>');
+});
